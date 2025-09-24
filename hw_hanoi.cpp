c637e9a6e66365e0570c36c6f031fa7f8984b200
kpwang2005 <32416156+kpwang2005@users.noreply.github.com>
2025-09-24 09:37:39 +0800

    Update hw_hanoi.cpp

--- hw_hanoi.cpp
+++ hw_hanoi.cpp
@@ -1,8 +1,9 @@
 #include <vector>
+#include <iostream>
 
 using namespace std;
 
-auto rotate_lf = [](auto sample) {
+auto rotate_lf (auto sample) {
     int n = sample.size();
     vector<vector<int>> result(n, vector<int>(n, 0));
 
@@ -15,7 +16,7 @@ auto rotate_lf = [](auto sample) {
     return result;
 };
 
-auto mirror = [](auto sample) {
+auto mirror (auto sample) {
     int n = sample.size();
     vector<vector<int>> result(n, vector<int>(n, 0));
 
@@ -28,7 +29,13 @@ auto mirror = [](auto sample) {
     return result;
 };
 
-auto align_lf = [](auto sample) {
+auto zero_rt (auto sample) {
+    int n = sample.size();
+    vector<vector<int>> result(n, vector<int>(n, 0));
+    return result;
+};
+
+auto align_lf (auto sample) {
     int n = sample.size();
     vector<vector<int>> result(n, vector<int>(n, 0));
 
@@ -45,10 +52,71 @@ auto align_lf = [](auto sample) {
     return result;
 };
 
+auto connect_lr (auto lf, auto rt) {
+    int n = lf.size();
+    vector<vector<int>> result = lf;
+    for(int i=0; i<n; i++) {
+        result[i].push_back(0);
+        result[i].insert(result[i].end(),rt[i].begin(),rt[i].end());
+    }
+    return result;
+}
+auto connect_ud (auto up, auto dn) {
+    int n = up.size();
+    vector<vector<int>> result = up;
+    vector<int> row = {1};
+    row.insert(row.end(),n-1,0);
+    row.push_back(1);
+    row.insert(row.end(),n,0);
+    result.push_back(row);
+    result.insert(result.end(),dn.begin(),dn.end());
+    return result;
+}
+
+vector<vector<int>> sample = {{1,0,0}, {1, 1, 0}, {1, 0, 1}};
+
+auto hanoi (int n) {
+    if(n==1) return sample;
+
+    auto last = hanoi(n-1);
+    auto zero = zero_rt(last);
+    auto rota = rotate_lf(last);
+    auto dnlf = mirror(rota);
+    auto dnrt = align_lf(rota);
+    auto up_h = connect_lr(last, zero);
+    auto dn_h = connect_lr(dnlf, dnrt);
+    return connect_ud(up_h, dn_h);
+}
+
+void draw (auto sample) {
+    int n = sample.size();
+    for(int i=0; i<n; i++) {
+        for(int j=0; j<n-1-i; j++)
+            cout << " ";
+        if(i%2 == 0) {
+            for(int j=0; j<n; j++) {
+                if(j%2 == 0)
+                    cout << (sample[i][j] ? "@" : " ");
+                else
+                    cout << (sample[i][j] ? "---" : "   ");
+            }
+
+        } else {
+            for(int j=0; j<n; j++) {
+                if(j%2 == 0)
+                    cout << (sample[i][j] ? "/ " : "  ");
+                else
+                    cout << (sample[i][j] ? "\\ " : "  ");
+            }
+        }    
+        cout << endl;       
+    }
+}
+
 int main() {
-    vector<vector<int>> sample = {{1,0,0}, {1, 1, 0}, {1, 0, 1}};
-    auto rotated = rotate_lf(sample);
-    auto mirrored = mirror(rotated);
-    auto aligned = align_lf(rotated);
+    int n;
+    cin >> n;
+    auto result = hanoi(n);
+    draw(result);
     return 0;
 }
